421668c61b2e28c6756b873ca96840a91f9ecf85
Arvind Suthar <arvindsuthar198@gmail.com>
2023-06-22 19:31:58 +0530

    db connection

--- config/config.ts
+++ config/config.ts
@@ -1,1 +1,3 @@
 export const serverPort = 3000;
+
+export const databaseUrl = 'mongodb://localhost:27017/task1';
--- server.ts
+++ server.ts
@@ -1,7 +1,12 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import { serverPort } from './config/config';
 
 const app = express();
+
+// Create MongoDB connection
+mongoose.connect('mongodb://localhost:27017/task1');
+
 
 // Start the server
 app.listen(serverPort, () => {
